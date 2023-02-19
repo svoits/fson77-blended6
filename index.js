f49d750a046202188ccefb259fdc7aed89e23a91
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-02-19 13:14:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,34 @@
  *? інакше відобразити:"Не знаєте? ECMAScript!"
  */
 
-const answer = prompt("Яка офіційна назва JavaScript?").trim();
-const officialJSName = "ECMAScript";
+// const answer = prompt("Яка офіційна назва JavaScript?").trim();
+// const officialJSName = "ECMAScript";
 
-if (answer === officialJSName) {
-  alert("Вірно!");
-} else {
-  alert("Не знаєте? ECMAScript!");
-}
+// if (answer === officialJSName) {
+//   alert("Вірно!");
+// } else {
+//   alert("Не знаєте? ECMAScript!");
+// }
+
+
+/**
+ *? Напишіть програму, яка отримає від користувача
+ *? число (кількість хвилин) та виведе в консоль
+ *? рядок у форматі годин та хвилин
+ *? https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ *? 70 === 01:10
+ */
+
+const numberFromUser = prompt("Введіть число");
+
+
+ function minutesToHour(number){
+  let hours = String(Math.floor(number / 60)).padStart(2,"0");
+
+  let minutes = String(number % 60).padStart(2,"0");
+  console.log(`${hours}:${minutes}`);
+  return `${hours}:${minutes}`;
+ }
+
+ minutesToHour(numberFromUser);
+ 

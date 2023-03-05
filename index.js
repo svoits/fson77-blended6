f319b4d0ad74699a52a017aea41501c7b66cbea1
Illia Shulha <ilyxa2836@gmail.com>
2023-03-05 15:51:50 +0100

    task-8

--- index.js
+++ index.js
@@ -273,33 +273,34 @@
 //   .sort((a, b) => concerts[a] - concerts[b]);
 // console.log(filteredCities);
 
-
-
 const arr = [
-{ name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
-{ name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
-{ name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
-{ name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
-{ name: "Pierre", surname: "Curie", born: 1859, dead: 1906, id: 5 },
-{ name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 6 },
-{ name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 7 },
-{ name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 8 },
-{ name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 9 },
-{ name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 10 },
-{ name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 11 },
-{ name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 12 },
-{ name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 13 }
+  { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+  { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+  { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+  { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+  { name: "Pierre", surname: "Curie", born: 1859, dead: 1906, id: 5 },
+  { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 6 },
+  { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 7 },
+  { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 8 },
+  { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 9 },
+  { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 10 },
+  { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 11 },
+  { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 12 },
+  { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 13 },
 ];
 // - Получить массив ученых, родившихся в 19 веке.
 
-const result = arr.filter(person => person.born > 1800 && person.born <= 1900);
-console.log(result);
+// const result = arr.filter(person => person.born > 1800 && person.born <= 1900);
+// console.log(result);
 
-const years = arr.reduce((sum, person) => sum + (person.dead - person.born) ,0);
-console.log(years);
-
+// const years = arr.reduce((sum, person) => sum + (person.dead - person.born) ,0);
+// console.log(years);
 
 // - Отсортировать ученых по алфавиту.
+
+const result = [...arr].sort(({ name: a }, { name: b }) => a.localeCompare(b));
+console.log(result);
+
 // - Отсортировать ученых по количеству прожитых лет.
 // - Удалить из массива ученых, родившихся в 15, 16 или 17 веках.
 // - Найти ученого, который родился позже всех.

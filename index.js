d092f8e7d1da3100bd7061975472dc9da5258740
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-03-05 14:18:19 +0100

    1

--- index.js
+++ index.js
@@ -144,10 +144,35 @@
  *? Если объект salaries пуст, то результат должен быть 0
  */
 
- const salaries = {
-  Mango: 100,
-  Poly: 160,
-  Ajax: 1470,
-};
-const valuesSalaries = Object.values(salaries).reduce((sum, alt) => sum += alt, 0);
-console.log(valuesSalaries);
+//  const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+// const valuesSalaries = Object.values(salaries).reduce((sum, alt) => sum += alt, 0);
+// console.log(valuesSalaries);
+
+/**
+ *? Напишіть ф-цію calcTotalPrice(stones, stonesName),
+ *? яка приймає масив об'єктів та
+ *? рядок під назвою каменю.
+ *? Функція шукає та повертає загальну вартість каменів
+ *? з таким ім'ям, ціною та кількістю з об'єкта
+ */
+
+const stones = [
+  { name: "Изумруд", price: 1300, quantity: 4 },
+  { name: "Бриллиант", price: 2700, quantity: 6 },
+  { name: "Сапфир", price: 400, quantity: 7 },
+  { name: "Щебень", price: 150, quantity: 100 },
+];
+
+function calcTotalPrice(array, stonesName) {
+  const unicueStone = array.find((stone) => stone.name === stonesName);
+  console.log(unicueStone);
+  if (!unicueStone) {
+    return "Такого каменю немає";
+  }
+  return unicueStone.price * unicueStone.quantity;
+}
+console.log(calcTotalPrice(stones, "ffghhh"));
